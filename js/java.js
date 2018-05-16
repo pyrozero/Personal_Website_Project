305e43756215288de54323808e6a994353616569
pyrozero <pyro.zero@hotmail.com>
2018-05-16 19:37:53 +0700

    เอาไฟล์javaออกจากhtmlมาไว้ในjava.js ,ปรับขนาดgrid

--- js/java.js
+++ js/java.js
@@ -1,3 +1,20 @@
-function scrollWin() {
-    window.scrollBy(0, 1000);
+
+function scrollWin(x,y) {
+    window.scrollBy(x,y);
+}
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("btnBackIndex").style.display = "block";
+    } else {
+        document.getElementById("btnBackIndex").style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
 }
