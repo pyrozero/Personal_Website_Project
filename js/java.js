f8a5c6d03c073ee8e3ecfe70b7196f685cb16315
pyrozero <pyro.zero@hotmail.com>
2018-05-18 01:39:44 +0700

    เพิ่มข้อมูล css  menubar ของpage

--- js/java.js
+++ js/java.js
@@ -1,3 +1,18 @@
+var myIndex = 0;
+carousel();
+
+function carousel() {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    for (i = 0; i < x.length; i++) {
+       x[i].style.display = "none";  
+    }
+    myIndex++;
+    if (myIndex > x.length) {myIndex = 1}    
+    x[myIndex-1].style.display = "block";  
+    setTimeout(carousel, 1500); // Change image every 2 seconds
+}
+
 
 function scrollWin(x,y) {
     window.scrollBy(x,y);
@@ -18,3 +33,5 @@ function topFunction() {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
 }
+
+
